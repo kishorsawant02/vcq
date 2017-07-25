5b29588b0054729be16e11b020bf587002612f2b
Kishor Sawant <kishor.sawant@amdocs.com>
2017-07-25 19:23:50 +0800

    admin changes

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(function (req, res, next) {
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
+  res.locals.isAdmin = (req.user == '8055999123')?true:false;
   next();
 });
 //router
